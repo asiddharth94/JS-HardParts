5733a8628ab223afe8bc7728bca701028cd99e26
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2020-09-09 23:58:46 +0530

    Part 2

--- oop-1.js
+++ oop-1.js
@@ -21,7 +21,6 @@ const user1 = {
 };
 
 const result1 = user1.incrementScore();
-console.log(`for type one ${result1}`);
 
 //type two - creating an empty object and assigning properties/methods to it using dot notation
 
@@ -49,5 +48,71 @@ user3.incrementScore = function() {
 
 //At this point , we're writing very repetitive code, which violates our DRY priniciple
 
+// SOLUTION - 1
 //What do we do then? FUNCTIONS - wrapping a set of instructions which can be used over and again, eliminating repetitve code. WRITE ONCE, CALL MANY TIMES!
-//In our case, we can wrap our creation of an object in a function, then we can call that function and fill in the properties, and then return the complete object and then store it in a variable
+//In our case, we can wrap our creation of an object in a function, then we can call that function and fill in the individual properties, and then return the complete object and then store it in a variable
+
+function createUserOne(name, score) {
+    
+    const newUser = {};
+    newUser.name = name;
+    newUser.score = score;
+    newUser.incrementScore = function() {
+        return ++this.score;
+    }
+
+    return newUser;
+}
+
+const user4 = createUserOne('Amy', 12);
+const user5 = createUserOne('Megan', 15);
+
+user4.incrementScore();
+user5.incrementScore();
+
+//So, solution 1 is doing our job(creating objects with bundling data and functionality
+//together) but it is fundamentally unusable. Now, we'll see why!
+
+//Because each time we create an user object, we allocate space in our computer's memory for
+//the user's data and functions. And as we see, we are making same function for each user
+//object separately. So, this solution is fundamentally unusable as it is creating a copy
+//of the same function for each object. Just think about the space allocation for a huge amount
+//of data! (e.g if thousands of users have hundreds of functions attached to each one of them -
+//we'll be just wasting space, right? as all objects have exactly the same functions)
+
+//DO WE HAVE A BETTER SOLUTION? YES, WE DO! 
+
+//SOLUTION - 2
+
+//store all our functions in one object , and if the interpreter doesn't find the methods on the
+//objects on which it is called, it could look up to our object(the one which has the functions),
+//if it's there - LET US CREATE THAT LINK!
+
+
+const functionStore = {
+    incrementScore: function() {
+        return ++this.score;
+    }
+};
+
+function createUserTwo(name, score) {
+    const newUser = Object.create(functionStore); //refer Line:42 - this line creates an empty object
+                // 'newUser' but when we create an object using Object.create, the newly created
+                // object gets a hidden property '__proto__', which creates a 'bond' to the 
+                // argument which we pass, functionStore in this case.
+    newUser.name = name;
+    newUser.score = score;
+    return newUser;
+}
+
+/**
+ * MDN definition - 
+ * The Object.create() method creates a new object, using an existing object as the prototype of the
+ * newly created object.
+ */
+
+ //This built-in feature is called PROTOTYPE CHAIN
+
+const user6 = createUserTwo('Mark', 22);
+console.log(user6.incrementScore()); //returns 23 - once it could not find the incrementScore 
+//function on user6, it looks up in the functionStore 
