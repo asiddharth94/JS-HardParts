5850a4820726ebda5a6b123c4fdcfaaaa5e2a8b0
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2020-09-26 01:13:04 +0530

    the syntactic sugar - 'class'

--- oop-2.js
+++ oop-2.js
@@ -12,7 +12,9 @@ UserCreator.prototype.increment = function() {
     //     console.log('inner func', this); // refers to window object
     // }
 
-    const add1 = () => this.score++;
+    const add1 = () => this.score++; //in arrow functions, 'this' is lexically scoped, i.e, it refers
+    //to where it is defined. => THE 'THIS' INSIDE THE ARROW FUNCTION'S EXECUTION CONTEXT WILL REFER TO 
+    //WHATEVER WAS THE VALUE OF 'THIS', WHEN THE ARROW FUNCTION WAS DEFINED.
 
     add1();
 }
@@ -24,3 +26,43 @@ UserCreator.prototype.login = function() {
 const user1 = new UserCreator('Eva', 10);
 
 user1.increment();
+
+//NOTE - in simple terms, lexical scoping means, where I was saved determines something about me
+// when I get called, 'this' in our case
+
+// the syntactic sugar - CLASS
+
+class UserCreator1 {
+    constructor(name, score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    increment() {
+        this.score++;
+    }
+
+    login() {
+        console.log("Login");
+    }
+}
+
+const user2 = new UserCreator1('Will', "12");
+
+user2.login();
+
+//under the hood, this example is doing exact same as what we did in first example. exact same!
+//here also, when we declare UserCreator1, it automatically gets an object attached to it (similarly,
+//like we had for our function-object combos) , and that object has a property "prototype" which was our
+//empty object for the shared functions.
+
+//ON A VERY HIGH LEVEL, just think of it as - the function bit of our func-obj combo,
+//goes inside constructor & the object bit, where we store our shared functions , 
+//are just listed out inside the class. These listed funcs are then grabbed and then put to the
+//'prototype' object automatically for us
+
+// Refresher => the 'prototype' object - here, we put all the functions that we want the returned
+// objects from calling UserCreator to have access to
+
+//When it comes to subclassing, classes implements some things under the hood quite differently,
+//we'll see that in next sections. till this point, its exact same (i'm repeating this a lot :D)
